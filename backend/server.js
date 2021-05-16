4ed42784330818c58efc1452d1f92a807ddc2ff8
GuzmanAndrew <ramirezandrew172@gmail.com>
2021-05-15 19:31:05 -0500

    Change server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,17 @@ app.get('/tweets', (req, res) => {
     }).catch(error => {
         res.send(error);
     })
-})
+});
+
+app.get('/search/:word', (req, res) => {
+    const params = {count: 9, tweet_mode: 'extended', q: req.params.word, result_type: 'recent', lang: 'es'};
+    config.apiClient.get('search/tweets', params)
+    .then(results => {
+        res.send(results);
+    }).catch(error => {
+        res.send(error);
+    })
+});
 
 app.listen(config.port, () => {
     console.log('server running on port ' + config.port );
